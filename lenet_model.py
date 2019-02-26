6dc481731b7579b711d8f014837ec1e7003b375c
Ana Nora Evans <ananevans@gmail.com>
2019-02-26 14:09:45 -0500

    changed input dimensions

--- lenet_model.py
+++ lenet_model.py
@@ -2,7 +2,7 @@ import data
 import numpy as np
             
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Cropping2D, BatchNormalization
 
 import tensorflow as tf
 tf.Session(config=tf.ConfigProto(log_device_placement=True))
@@ -10,15 +10,16 @@ tf.Session(config=tf.ConfigProto(log_device_placement=True))
 # used implementation from https://github.com/TaavishThaman/LeNet-5-with-Keras/blob/master/lenet_5.py
 def lenet():
     model = Sequential()
+    model.add(BatchNormalization(input_shape=(160,320,3)))
     # cropping
-    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(3,160,320)))
+    model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
     #Layer 1
     #Conv Layer 1
     model.add(Convolution2D(filters = 6, 
                      kernel_size = 5, 
                      strides = 1, 
                      activation = 'relu', 
-                     input_shape = (160,320,3)))
+                     input_shape = (90,320,3)))
     #Pooling layer 1
     model.add(MaxPooling2D(pool_size = 2, strides = 2))
     #Layer 2
