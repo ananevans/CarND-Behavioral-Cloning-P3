c014b7c8015d10e5b44380f410395d59d71ddf44
Ana Nora Evans <ananevans@gmail.com>
2019-02-28 17:52:41 -0500

    changed dropout rate

--- dave2-dropout.py
+++ dave2-dropout.py
@@ -16,8 +16,8 @@ def dave2():
     # cropping
     model.add(Cropping2D(cropping=((55,20), (0,0)), input_shape=(160,320,3)))
 
-    #model.add(BatchNormalization(input_shape=(160,320,3)))
-    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    model.add(BatchNormalization(input_shape=(160-55-20,320,3)))
+    #model.add(Lambda(lambda x: (x / 255.0) - 0.5))
     
     model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", padding="valid"))
     
@@ -34,19 +34,19 @@ def dave2():
     
     # dense 1164
     model.add(Dense(1164))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
         
     # dense 100
     model.add(Dense(100))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     
     # dense 50
     model.add(Dense(50))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     
     # dense 10
     model.add(Dense(10))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
     
     # dense 1
     model.add(Dense(1))
