944909bbbc124e3cd1a70592f2e5464eebb81e46
Ana Nora Evans <ananevans@gmail.com>
2019-03-05 16:31:37 -0500

    clean up

--- dave2_dropout.py
+++ dave2_dropout.py
@@ -61,7 +61,7 @@ def dave2():
     
     return model
 
-train.train( dave2(), 'dave2-dropout', True, False, True, 20, 0.5)
-train.train( dave2(), 'dave2-dropout', True, True, True, 20, 0.5)
-train.train( dave2(), 'dave2-dropout', False, False, True, 20, 0.5)
-train.train( dave2(), 'dave2-dropout', False, True, True, 20, 0.5)
+train.train( dave2(), 'dave2-dropout', True, False, True, 10, 0.5)
+#train.train( dave2(), 'dave2-dropout', True, True, True, 10, 0.5)
+#train.train( dave2(), 'dave2-dropout', False, False, True, 10, 0.5)
+#train.train( dave2(), 'dave2-dropout', False, True, True, 10, 0.5)
